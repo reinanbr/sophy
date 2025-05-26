eb0338a3c4169e109dc66553606baff17690f557
reinanbr <perseu912@gmail.com>
2025-05-26 12:00:41 -0300

    v0.1.4

--- sophi-cli/src/main.rs
+++ sophi-cli/src/main.rs
@@ -3,11 +3,12 @@ use sophi::methods::raphson;
 
 
 fn main() {
-    
-    let f = |x: f64| x * x - 2.0; // Function: x^2 - 2
-    let df = |x: f64| 2.0 * x; // Derivative: 2x
-    let root = raphson(1.0, &f, &df, 1e-10, 100);
-    println!("Root of x^2 - 2 = {}", root);
-    assert!((root - 1.4142135623730951).abs() < 1e-10); // sqrt(2) is approximately 1.4142135623730951
-    println!("Test passed!");
+    let f = |x: f64| x * x - 2.0;
+    let df = |x: f64| 2.0 * x;
+    let root = raphson(1.0, f, df, 1e-10, 100);
+
+    println!("Root of x^2 - 2 = {root}");
+
+    assert!((root - std::f64::consts::SQRT_2).abs() < 1e-10);
 }
+
--- sophi-lib/src/lib.rs
+++ sophi-lib/src/lib.rs
@@ -14,10 +14,13 @@ mod tests {
 
 
     #[test]
+#[test]
     fn test_raphson() {
         let f = |x: f64| x * x - 2.0; // Function: x^2 - 2
-        let df = |x: f64| 2.0 * x; // Derivative: 2x
-        let root = methods::raphson::raphson(1.0, &f, &df, 1e-10, 100);
-        assert!((root - 1.4142135623730951).abs() < 1e-10); // sqrt(2) is approximately 1.4142135623730951
+        let df = |x: f64| 2.0 * x;    // Derivative: 2x
+        let root = methods::raphson::raphson(1.0, f, df, 1e-10, 100);
+
+        assert!((root - std::f64::consts::SQRT_2).abs() < 1e-10);
     }
+
 }
